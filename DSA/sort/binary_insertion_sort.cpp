5b16abdd6074eab1c63a96df4a4fb9d2fb7192fe
Đoàn Anh Tuấn <99138995+mairesol@users.noreply.github.com>
2022-04-09 21:19:14 +0700

    trash

--- DSA/sort/binary_insertion_sort.cpp
+++ DSA/sort/binary_insertion_sort.cpp
@@ -5,26 +5,27 @@ using namespace std;
 
 int binary_search(int arr[], int left, int right, int x)
 {
-    if (right >= left)
-    {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == x)
-            return mid;
-        if (arr[mid] > x)
-            return binary_search(arr, left, mid - 1, x);
-        return binary_search(arr, mid + 1, right, x);
-    }
-    return -1;
+    if (right <= left)
+        return (x > arr[left]) ? (left + 1) : left;
+
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == x)
+        return mid + 1;
+    if (arr[mid] > x)
+        return binary_search(arr, left, mid - 1, x);
+    return binary_search(arr, mid + 1, right, x);
 }
 
 void binary_insertion_sort(int arr[], int n)
 {
-    int key, j;
+    int key, loc, j;
     for (int i = 1; i < n; i++)
     {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > key)
+        loc = binary_search(arr, 0, j, key);
+
+        while (j >= loc)
         {
             arr[j + 1] = arr[j];
             j--;
